5ae177df24061dbb6f184ccb55695a0abe06763d
Calvin <37385@ma-web.nl>
2023-10-20 12:09:16 +0200

    text verandert

--- js/main.js
+++ js/main.js
@@ -9,7 +9,10 @@ function basicHaiku()
     return ["Toward those short trees","We saw a batman descending","On a day in winter."]
 }
 //zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
-
+function basicHaiku1()
+{
+    return ["towards those animals","We saw a superman descending","On a day in summer."]
+}
 haikus = [
     basicHaiku()
 ]
